5783567829ba5faf47bf4dd5b47c2d8ee5877754
Dylan <dbreon@ucla.edu>
2021-05-13 12:33:37 -0700

    changedup script and added heath check for postgres container

--- dbInit/01-init.sh
+++ dbInit/01-init.sh
@@ -2,16 +2,12 @@
 set -e
 export PGPASSWORD=$POSTGRES_PASSWORD;
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
-  CREATE DATABASE $APP_DB_NAME;
-  GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO $APP_DB_USER;
-  \connect $APP_DB_NAME $APP_DB_USER
   BEGIN;
     CREATE TABLE IF NOT EXISTS event (
-	  block_number bigint PRIMARY KEY NOT NULL,
+	  block_number bigint NOT NULL PRIMARY KEY,
 	  compound real not null,
-	  dsr real not null,
-      aave real not null,
+    dsr real not null, 
+    aave real not null, 
 	  created timestamp default current_timestamp
 	);
   COMMIT;

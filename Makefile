aa196744d1c4d29efd15e2ee6737ae28a9da0048
Cooper Maruyama <me@cooperm.com>
2021-05-13 13:11:55 -0700

    pg migration fix

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
+
+CONTAINER_PG := $(shell docker-compose ps -q db)
+
 run: build
 	docker compose up -d
 build:
 	docker compose build
 dev: build
-	docker compose up 
-clean: 
+	docker compose up
+clean:
 	docker compose down
+migrate:
+	docker exec -it ${CONTAINER_PG} /dbInit/01-init.sh
--- dbInit/01-init.sh
+++ dbInit/01-init.sh
@@ -6,8 +6,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE TABLE IF NOT EXISTS event (
 	  block_number bigint NOT NULL PRIMARY KEY,
 	  compound real not null,
-    dsr real not null, 
-    aave real not null, 
+    dsr real not null,
+    aave real not null,
 	  created timestamp default current_timestamp
 	);
   COMMIT;
